53425ad72eb5b436d503fadb6884b4ee2db75b7e
Marat <markbass82@gmail.com>
2020-10-14 16:22:06 +0300

    'Создание репозитория, и установка приложения React'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        <h1>Create Amazon-clone with React!</h1>
+    </div>
+  );
+}
+
+export default App;
